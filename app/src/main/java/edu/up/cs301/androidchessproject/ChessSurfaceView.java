b32a49650e94b9a6424a7c0fb4082b98127c1243
CarrC2021 <carrc21@up.edu>
2020-03-27 16:52:01 -0700

    beginning of the gui creation

--- app/src/main/java/edu/up/cs301/androidchessproject/ChessSurfaceView.java
+++ app/src/main/java/edu/up/cs301/androidchessproject/ChessSurfaceView.java
@@ -1,6 +1,10 @@
 package edu.up.cs301.androidchessproject;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +14,15 @@ import edu.up.cs301.game.GameFramework.utilities.FlashSurfaceView;
 public class ChessSurfaceView extends FlashSurfaceView implements View.OnTouchListener, View.OnClickListener {
     public ChessSurfaceView(Context context) {
         super(context);
+        init();
+    }
+    public ChessSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }// ctor
+
+    private void init() {
+        setBackgroundColor(Color.YELLOW);
     }
 
     @Override
@@ -23,5 +36,13 @@ public class ChessSurfaceView extends FlashSurfaceView implements View.OnTouchLi
     @Override
     public boolean onTouch(View v, MotionEvent event) {
         return false;
+    }
+
+    @Override
+    public void onDraw(Canvas c) {
+        super.onDraw(c);
+        Paint p = new Paint();
+        p.setColor(Color.BLUE);
+        c.drawCircle(300,300,200,p);
     }
 }
