1a74d4b45ec61f08b047ff1864244e5eba888bbf
CarrC2021 <carrc21@up.edu>
2020-03-30 16:27:16 -0700

    tested get and set board

--- app/src/test/java/edu/up/cs301/androidchessproject/ChessStateTest.java
+++ app/src/test/java/edu/up/cs301/androidchessproject/ChessStateTest.java
@@ -2,6 +2,14 @@ package edu.up.cs301.androidchessproject;
 
 import org.junit.Test;
 
+import edu.up.cs301.androidchessproject.boardandpieces.ChessSquare;
+import edu.up.cs301.androidchessproject.boardandpieces.GameBoard;
+import edu.up.cs301.androidchessproject.boardandpieces.ChessPiece;
+import edu.up.cs301.androidchessproject.boardandpieces.Pawn;
+import edu.up.cs301.game.GameFramework.infoMessage.TimerInfo;
+import edu.up.cs301.game.GameFramework.utilities.GameTimer;
+import edu.up.cs301.game.GameFramework.utilities.Tickable;
+
 import static org.junit.Assert.*;
 
 public class ChessStateTest {
@@ -33,7 +41,27 @@ public class ChessStateTest {
 
     @Test
     public void setBoard() {
+        //it says here that is requires a tickable object but this is an interface I guess I just do not understand this class structure.
+        //the ChessState constructor requires two TimerInfo objects so before I can test any of the methods this needs to be done.
+        TimerInfo timer1 = new TimerInfo(new GameTimer(new Tickable() {
+            @Override
+            public void tick(GameTimer timer) {
 
+            }
+        }));
+        TimerInfo timer2 = new TimerInfo(new GameTimer(new Tickable() {
+            @Override
+            public void tick(GameTimer timer) {
+
+            }
+        }));
+        ChessState state = new ChessState(timer1, timer2);
+
+        GameBoard board = new GameBoard();
+        board.getSquares()[23].setPiece(new Pawn(23, 1));
+        state.setBoard(board);
+
+        assertEquals(23, state.getBoard().getSquares()[23].getPiece().getLocation());
     }
 
     @Test
