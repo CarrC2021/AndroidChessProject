9194e580a80a02ec3017ea906e5d7f91d432347b
CarrC2021 <carrc21@up.edu>
2020-04-11 10:04:04 -0700

    easy player updated, should not be an infinite loop anymore

--- app/src/main/java/edu/up/cs301/androidchessproject/ChessComputerPlayerEasy.java
+++ app/src/main/java/edu/up/cs301/androidchessproject/ChessComputerPlayerEasy.java
@@ -11,6 +11,8 @@
 
 package edu.up.cs301.androidchessproject;
 
+import android.app.VoiceInteractor;
+
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -62,14 +64,12 @@ public class ChessComputerPlayerEasy extends GameComputerPlayer {
     protected void receiveInfo(GameInfo info) {
         sleep(2);
 //need to add an overall loop over all of this.
-        if (info instanceof NotYourTurnInfo){
+        if (info instanceof NotYourTurnInfo) {
             return;
 
-        }
-
-        else if (info instanceof IllegalMoveInfo || info instanceof ChessState){
+        } else if (info instanceof IllegalMoveInfo || info instanceof ChessState) {
             if (info instanceof ChessState) {
-                setState((ChessState)info);
+                setState((ChessState) info);
             }
             if (state.getPlayerToMove() != playerNum) return;
 
@@ -84,177 +84,123 @@ public class ChessComputerPlayerEasy extends GameComputerPlayer {
                     Logger.log("no black", "no black pieces");
                     return;
                 }
-                Logger.log("blackSize", "#black pieces: "+BlackPieces.size());
-                randPiece = BlackPieces.get(randomIntWithinBounds(0, BlackPieces.size()-1));
-            }
-            else {
+                Logger.log("blackSize", "#black pieces: " + BlackPieces.size());
+                randPiece = BlackPieces.get(randomIntWithinBounds(0, BlackPieces.size() - 1));
+            } else {
                 if (WhitePieces.size() <= 0) {
                     Logger.log("no white", "no white pieces");
                     return;
                 }
-                Logger.log("whiteSize", "#white pieces: "+WhitePieces.size());
-                randPiece = WhitePieces.get(randomIntWithinBounds(0, WhitePieces.size()-1));
+                Logger.log("whiteSize", "#white pieces: " + WhitePieces.size());
+                randPiece = WhitePieces.get(randomIntWithinBounds(0, WhitePieces.size() - 1));
             }
-            int row = randomIntWithinBounds(MIN,MAX);
-            int col = randomIntWithinBounds(MIN,MAX);
-            if(randPiece instanceof Pawn){
+            int row = randomIntWithinBounds(MIN, MAX);
+            int col = randomIntWithinBounds(MIN, MAX);
+            if (randPiece instanceof Pawn) {
                 Logger.log("CPE pawn",
                         "computer player chooses pawn to move");
-                while(!Pawn.isValidPawnMove(state,randPiece,randPiece.getRow(),randPiece.getCol(),row,col)){
-                    row = randomIntWithinBounds(MIN,MAX);
-                    col = randomIntWithinBounds(MIN,MAX);
-                    Logger.log("seek pawn",
-                            "looking for a pawn move");
+                for (int i = 0; i < 20; i++) {
+                    if (!Pawn.isValidPawnMove(state, randPiece, randPiece.getRow(), randPiece.getCol(), row, col)) {
+                        row = randomIntWithinBounds(MIN, MAX);
+                        col = randomIntWithinBounds(MIN, MAX);
+                        Logger.log("seek pawn",
+                                "looking for a pawn move");
+                    } else {
+                        break;
+                    }
                 }
             }
-            if(randPiece instanceof Queen){
+            if (randPiece instanceof Queen) {
                 Logger.log("CPE queen",
                         "computer player chooses queen to move");
-                row = randomIntWithinBounds(MIN,MAX);
-                col = randomIntWithinBounds(MIN,MAX);
-                while(!Queen.isValidQueenMove(state, randPiece.getRow(), randPiece.getCol(), row, col)){
-                    row = randomIntWithinBounds(MIN,MAX);
-                    col = randomIntWithinBounds(MIN,MAX);
-                    Logger.log("seek queen",
-                            "looking for a queen move");
+                row = randomIntWithinBounds(MIN, MAX);
+                col = randomIntWithinBounds(MIN, MAX);
+
+                for (int i = 0; i < 20; i++) {
+                    if (!Queen.isValidQueenMove(state, randPiece.getRow(), randPiece.getCol(), row, col)) {
+                        row = randomIntWithinBounds(MIN, MAX);
+                        col = randomIntWithinBounds(MIN, MAX);
+                        Logger.log("seek queen",
+                                "looking for a queen move");
+                    } else {
+                        break;
+                    }
                 }
             }
-            if(randPiece instanceof Knight){
+            if (randPiece instanceof Knight) {
                 Logger.log("CPE knight",
                         "computer player chooses knight to move");
-                row = randomIntWithinBounds(MIN,MAX);
-                col = randomIntWithinBounds(MIN,MAX);
-                while(!Knight.isValidKnightMove(state, randPiece.getRow(), randPiece.getCol(), row, col)){
-                    row = randomIntWithinBounds(MIN,MAX);
-                    col = randomIntWithinBounds(MIN,MAX);
-                    Logger.log("seek knight",
-                            "looking for a knight move");
+                row = randomIntWithinBounds(MIN, MAX);
+                col = randomIntWithinBounds(MIN, MAX);
+                for (int i = 0; i < 20; i++) {
+                    if (!Knight.isValidKnightMove(state, randPiece.getRow(), randPiece.getCol(), row, col)) {
+                        row = randomIntWithinBounds(MIN, MAX);
+                        col = randomIntWithinBounds(MIN, MAX);
+                        Logger.log("seek knight",
+                                "looking for a knight move");
+                    } else {
+                        break;
+                    }
                 }
             }
-            if(randPiece instanceof Bishop){
+            if (randPiece instanceof Bishop) {
                 Logger.log("CPE bishop",
                         "computer player chooses bishop to move");
-                row = randomIntWithinBounds(MIN,MAX);
-                col = randomIntWithinBounds(MIN,MAX);
-                //make this into a for loop of i = 20, so it does not search for
-                //possible locations to move indefinitely. If it cannot find a
-                //valid move, it should randomly choose a new piece to move.
-                //if a valid move is found, exit out of the loop and moveaction.
-                while(!Bishop.isValidBishopMove(state, randPiece.getRow(), randPiece.getCol(), row, col)){
-                    row = randomIntWithinBounds(MIN,MAX);
-                    col = randomIntWithinBounds(MIN,MAX);
-                    Logger.log("seek bishop",
-                            "looking for a bishop move");
+                row = randomIntWithinBounds(MIN, MAX);
+                col = randomIntWithinBounds(MIN, MAX);
+
+                for (int i = 0; i < 20; i++) {
+                    if (!Bishop.isValidBishopMove(state, randPiece.getRow(), randPiece.getCol(), row, col)) {
+                        row = randomIntWithinBounds(MIN, MAX);
+                        col = randomIntWithinBounds(MIN, MAX);
+                        Logger.log("seek bishop",
+                                "looking for a bishop move");
+                    }
                 }
             }
-            if(randPiece instanceof King){
+            if (randPiece instanceof King) {
                 Logger.log("CPE king",
                         "computer player chooses king to move");
-                row = randomIntWithinBounds(MIN,MAX);
-                col = randomIntWithinBounds(MIN,MAX);
-                //make this into a for loop of i = 20, so it does not search for
-                //possible locations to move indefinitely. If it cannot find a
-                //valid move, it should randomly choose a new piece to move.
-                //if a valid move is found, exit out of the loop and moveaction.
-                while(!King.isValidKingMove(state, randPiece.getRow(), randPiece.getCol(), row, col)){
-                    row = randomIntWithinBounds(MIN,MAX);
-                    col = randomIntWithinBounds(MIN,MAX);
-                    Logger.log("seek king",
-                            "looking for a \n" +
-                                    "                    Logger.log(\"seek knight\",\n" +
-                                    "                            \"looking for a knight move\"); move");
+                row = randomIntWithinBounds(MIN, MAX);
+                col = randomIntWithinBounds(MIN, MAX);
+
+                for (int i = 0; i < 20; i++) {
+                    if (!King.isValidKingMove(state, randPiece.getRow(), randPiece.getCol(), row, col)) {
+                        row = randomIntWithinBounds(MIN, MAX);
+                        col = randomIntWithinBounds(MIN, MAX);
+                        Logger.log("seek king",
+                                "looking for a \n" +
+                                        "                    Logger.log(\"seek king\",\n" +
+                                        "                            \"looking for a king move\"); move");
+                    }
+                    else {
+                        break;
+                    }
                 }
             }
-            if(randPiece instanceof Rook){
+            if (randPiece instanceof Rook) {
                 Logger.log("CPE rook",
                         "computer player chooses rook to move");
-                row = randomIntWithinBounds(MIN,MAX);
-                col = randomIntWithinBounds(MIN,MAX);
-                //make this into a for loop of i = 20, so it does not search for
-                //possible locations to move indefinitely. If it cannot find a
-                //valid move, it should randomly choose a new piece to move.
-                //if a valid move is found, exit out of the loop and moveaction.
-                while(!Rook.isValidRookMove(state, randPiece.getRow(), randPiece.getCol(), row, col)){
-                    row = randomIntWithinBounds(MIN,MAX);
-                    col = randomIntWithinBounds(MIN,MAX);
-                    Logger.log("seek rook",
-                            "looking for a rook move");
+                row = randomIntWithinBounds(MIN, MAX);
+                col = randomIntWithinBounds(MIN, MAX);
+                for (int i = 0; i < 20; i++) {
+                    if (!Rook.isValidRookMove(state, randPiece.getRow(), randPiece.getCol(), row, col)) {
+                        row = randomIntWithinBounds(MIN, MAX);
+                        col = randomIntWithinBounds(MIN, MAX);
+                        Logger.log("seek rook",
+                                "looking for a rook move");
+                    }
+                    else {
+                        break;
+                    }
                 }
             }
             ChessMoveAction action = new ChessMoveAction(this, randPiece, randPiece.getRow(), randPiece.getCol(), row, col);
             Logger.log("CPE move",
-                    "computer player move: "+randPiece.getRow()+" "+randPiece.getCol()+" "+row+" "+col);
+                    "computer player move: " + randPiece.getRow() + " " + randPiece.getCol() + " " + row + " " + col);
             game.sendAction(action);
             return;
         }
-//        else if (info instanceof ChessState){
-//
-//            setState((ChessState)info);
-//            fillPiecesList();
-//            ChessPiece randPiece;
-//            //take a random piece
-//            if (this.playerNum == 1) {
-//                randPiece = BlackPieces.get(randomIntWithinBounds(0, BlackPieces.size()-1));
-//            }
-//            else {
-//                randPiece = WhitePieces.get(randomIntWithinBounds(0, WhitePieces.size()-1));
-//            }
-//            int row = randomIntWithinBounds(MIN,MAX);
-//            int col = randomIntWithinBounds(MIN,MAX);
-//            if(randPiece instanceof Pawn){
-//                row = randomIntWithinBounds(MIN,MAX);
-//                col = randomIntWithinBounds(MIN,MAX);
-//                while(!Pawn.isValidPawnMove(state,randPiece,randPiece.getRow(),randPiece.getCol(),row,col)){
-//                    row = randomIntWithinBounds(MIN,MAX);
-//                    col = randomIntWithinBounds(MIN,MAX);
-//                }
-//            }
-//            if(randPiece instanceof Queen){
-//               row = randomIntWithinBounds(MIN,MAX);
-//                col = randomIntWithinBounds(MIN,MAX);
-//                while(!Queen.isValidQueenMove(state, randPiece.getRow(), randPiece.getCol(), row, col)){
-//                    row = randomIntWithinBounds(MIN,MAX);
-//                    col = randomIntWithinBounds(MIN,MAX);
-//                }
-//            }
-//            if(randPiece instanceof Knight){
-//                row = randomIntWithinBounds(MIN,MAX);
-//                col = randomIntWithinBounds(MIN,MAX);
-//                while(!Knight.isValidKnightMove(state, randPiece.getRow(), randPiece.getCol(), row, col)){
-//                    row = randomIntWithinBounds(MIN,MAX);
-//                    col = randomIntWithinBounds(MIN,MAX);
-//                }
-//            }
-//            if(randPiece instanceof Bishop){
-//                row = randomIntWithinBounds(MIN,MAX);
-//                col = randomIntWithinBounds(MIN,MAX);
-//                while(!Bishop.isValidBishopMove(state, randPiece.getRow(), randPiece.getCol(), row, col)){
-//                    row = randomIntWithinBounds(MIN,MAX);
-//                    col = randomIntWithinBounds(MIN,MAX);
-//                }
-//            }
-//            if(randPiece instanceof King){
-//                row = randomIntWithinBounds(MIN,MAX);
-//                col = randomIntWithinBounds(MIN,MAX);
-//                while(!King.isValidKingMove(state, randPiece.getRow(), randPiece.getCol(), row, col)){
-//                    row = randomIntWithinBounds(MIN,MAX);
-//                    col = randomIntWithinBounds(MIN,MAX);
-//                }
-//            }
-//            if(randPiece instanceof Rook){
-//               row = randomIntWithinBounds(MIN,MAX);
-//               col = randomIntWithinBounds(MIN,MAX);
-//                while(!Rook.isValidRookMove(state, randPiece.getRow(), randPiece.getCol(), row, col)){
-//                    row = randomIntWithinBounds(MIN,MAX);
-//                    col = randomIntWithinBounds(MIN,MAX);
-//                }
-//            }
-//            //make a random move using that piece, in the future we should grab a move from the validMoves list in the piece's data
-//            ChessMoveAction action = new ChessMoveAction(this, randPiece, randPiece.getRow(), randPiece.getCol(), row,col);
-//
-//
-//            game.sendAction(action);
-//        }
     }
 
     public int randomIntWithinBounds(int min, int max){
